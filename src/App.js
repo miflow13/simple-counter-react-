c30c813011e27a68d23328867573725a4f1df5c8
Mika <pizzafan513@gmail.com>
2023-12-06 13:24:33 -0500

    - Add React Router - Create new component for next game

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-// App.js
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Counter from './Counter';
 import Button from './Button';
 import Message from './Message';
 import WelcomeMessage from './WelcomeMessage';
+import Game2 from './Game2';  // Rename the imported component
 import './styles.css';
 
-function App() {
+const GamePage = ({ onReset }) => (
+  <React.Fragment>
+    <Counter count={0} />
+    <div className="button-container">
+      <Button onClick={onReset} label="Start Over" />
+      <Link to="/">
+        <Button label="Back to Home" />
+      </Link>
+    </div>
+  </React.Fragment>
+);
+
+const App = () => {
   const [count, setCount] = useState(0);
   const [msg, setMsg] = useState('');
 
@@ -29,20 +42,36 @@ function App() {
   };
 
   return (
-    <div>
-      <WelcomeMessage/>
-    <div id="container">
-      
-      <Counter count={count} />
-      <div className="button-container">
-        <Button onClick={onClick} label="Count" />
-        {count === 100 && <Button onClick={onReset} label="Start Over" />}
-        <Button onClick={onReset} label="Reset" />
-      </div>
-    </div>
-    <Message msg={msg} />
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <WelcomeMessage />
+              <div id="container">
+                <Counter count={count} />
+                <div className="button-container">
+                  {count < 100 && <Button onClick={onClick} label="Count" />}
+                  {count === 100 && (
+                    <>
+                      <Button onClick={onReset} label="Start Over" />
+                      <Link to="/game">
+                        <Button label="Next Game" />
+                      </Link>
+                    </>
+                  )}
+                  {count !== 100 && <Button onClick={onReset} label="Reset" />}
+                </div>
+              </div>
+              <Message msg={msg} />
+            </div>
+          }
+        />
+        <Route path="/game" element={<Game2 onReset={onReset} />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/Game2.js
@@ -0,0 +1,12 @@
+// Game2.js
+import React from 'react';
+import Counter from './Counter';
+import Button from './Button';
+
+const Game2 = ({ onReset }) => (
+  <div>
+    <h1>New Game Will Go Here</h1>
+  </div>
+);
+
+export default Game2;
