0b631b20eff52c1b7d02b837c5913ed9ab582230
Mika <pizzafan513@gmail.com>
2023-12-06 12:40:07 -0500

    Add styling. Changes to App.js, styles.css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import Counter from './Counter';
 import Button from './Button';
 import Message from './Message';
+import './styles.css';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,13 +12,11 @@ function App() {
   const onClick = () => {
     setCount(count + 1);
 
-    // Check if count is a milestone (e.g., 25, 50, 75, 100)
     const milestones = [25, 50, 75, 100];
     if (milestones.includes(count + 1)) {
       setMsg(`You have clicked ${count + 1} times!`);
     }
 
-    // Check if count is 100
     if (count + 1 === 100) {
       setMsg('You win! You have clicked 100 times!');
     }
@@ -28,18 +27,12 @@ function App() {
     setMsg('');
   };
 
-  const onStartOver = () => {
-    onReset();
-  };
-
   return (
-    <div className="App">
+    <div id="container">
       <Counter count={count} />
       <div className="button-container">
         <Button onClick={onClick} label="Click Here!" />
-        {count === 100 && (
-          <Button onClick={onStartOver} label="Start Over" />
-        )}
+        {count === 100 && <Button onClick={onReset} label="Start Over" />}
         <Button onClick={onReset} label="Reset" />
       </div>
       <Message msg={msg} />
